b86cb760895724126ace5309ecc1d63e4c909a33
saad <smsidrees@gmail.com>
2021-11-23 01:30:26 +0500

    updated user page

--- client/src/userPages/user.Page.js
+++ client/src/userPages/user.Page.js
@@ -1,12 +1,14 @@
 import react from "react";
 import { AppBar, Toolbar, Grid, Typography, Button, Divider } from "@mui/material";
 import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
 import { Link, NavLink } from "react-router-dom";
 import Header from "../components/header.Component.js"
 
 const UserPage = (props) => {
     const theme = useTheme();
     const classes = styles(theme);
+    const isXS = useMediaQuery(theme.breakpoints.only("xs"));
     return (<>
         <Header />
         <br />
@@ -17,12 +19,12 @@ const UserPage = (props) => {
                 item
                 container
                 justifyContent="center"
-                sm={4} md={3} lg={3} xl={3}
+               xs={7} sm={5} md={3} lg={3} xl={3}
             >
                 <img src="/assets/userImage.jpg" width="60%" style={classes.userImage} />
             </Grid>
 
-            <Grid item sm={2} md={1} lg={1} xl={1} >
+            <Grid item sm={2} md={1} lg={1} xl={1} sx={{display: isXS? "none": "block"}}>
                 <Divider orientation="vertical" sx={classes.divider}>
                     Details
                 </Divider>
@@ -33,8 +35,9 @@ const UserPage = (props) => {
                 container
                 direction="column"
                 justifyContent="center"
-                alignItems=""
-                sm={5} md={5} lg={5} xl={5}
+                alignItems="center"
+                xs={10} sm={5} md={5} lg={5} xl={5}
+                sx={classes.userDetailsMainContainer}
             >
 
                 <Grid item container direction="row" sx={classes.useerDetailsGrid}>
@@ -53,7 +56,7 @@ const UserPage = (props) => {
                 </Grid>
 
                 <Grid item container direction="row" sx={classes.useerDetailsGrid}>
-                    <Button variant="contained" sx={{ background: "#4d79ff" }}>
+                    <Button variant="contained"  sx={{ background: "#4d79ff" }}>
                         <Typography variant="caption">
                             Change
                         </Typography>
@@ -68,6 +71,8 @@ const UserPage = (props) => {
         <Divider sx={classes.divider} variant="middle" />
         <br />
         <br />
+
+        {/* TIMER CONTAINER GRID
         <Grid container justifyContent="center" >
 
             <Grid item xs={9} sm={9} md={9} lg={9} xl={9}>
@@ -83,6 +88,78 @@ const UserPage = (props) => {
             </Grid>
         </Grid>
 
+        GENERATED POINTS GRID
+        <Grid container justifyContent="center" >
+
+            <Grid item xs={9} sm={9} md={9} lg={9} xl={9}>
+                <Typography variant="h5" fontWeight="bold" align="center">
+                    Generated Points
+                </Typography>
+            </Grid>
+            
+            <Grid item xs={8} sm={5.5} md={4} lg={4} xl={4} sx={classes.timeRemainingGrid}>
+                <Typography variant="h4" fontWeight="bold" align="center">
+                    12 Points
+                </Typography>
+            </Grid>
+        </Grid> */}
+
+        {/* xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx */}
+
+        <Grid container justifyContent="space-evenly">
+
+            {/* TIMER CONTAINER GRID */}
+            <Grid
+                item
+                container
+                direction="column"
+                alignItems="center"
+                xs={11} sm={5.5} md={5.5} lg={5} xl={5}
+            >
+                <Grid item >
+                    <Typography variant="h5" fontWeight="bold" align="center">
+                        Next Feedback After
+                    </Typography>
+                </Grid>
+
+                <Grid
+                    item
+                    sx={classes.timeRemainingGrid}
+                >
+                    <Typography variant="h4" fontWeight="bold" align="center">
+                        13 : 23 : 54
+                    </Typography>
+                </Grid>
+
+            </Grid>
+
+            {/* GENERATED POINTS GRID */}
+            <Grid
+                item
+                container
+                direction="column"
+                alignItems="center"
+                xs={11} sm={5.5} md={5.5} lg={5} xl={5}
+            >
+                <Grid item >
+                    <Typography variant="h5" fontWeight="bold" align="center">
+                        Generated Points
+                    </Typography>
+                </Grid>
+
+                <Grid
+                    item
+                    sx={classes.timeRemainingGrid}
+                >
+                    <Typography variant="h4" fontWeight="bold" align="center">
+                        12 Points
+                    </Typography>
+                </Grid>
+
+            </Grid>
+
+        </Grid>
+
     </>)
 };
 
@@ -90,8 +167,8 @@ const UserPage = (props) => {
 const styles = (theme) => {
     return ({
         userImage: {
-            borderRadius: "50%", 
-            border: "10px solid #4d79ff", 
+            borderRadius: "50%",
+            border: "10px solid #4d79ff",
             padding: "3px"
         },
         divider: {
@@ -105,12 +182,21 @@ const styles = (theme) => {
         useerDetailsGrid: {
             marginY: "10px"
         },
-        timeRemainingGrid:{ 
-            padding: "30px", 
-            background: "#4d79ff", 
-            borderRadius: "20px", 
-            color: "white", 
-            marginTop:"10px" }
+        userDetailsMainContainer:{
+            [theme.breakpoints.only("xs")]:{
+                marginTop:"20px"
+            }
+        },
+        timeRemainingGrid: {
+            padding: "30px",
+            background: "#4d79ff",
+            borderRadius: "20px",
+            color: "white",
+            marginTop: "10px",
+            marginBottom: "30px",
+            boxShadow: "12px 12px 22px -13px #000000",
+            width:"80%"
+        }
     });
 };
 
